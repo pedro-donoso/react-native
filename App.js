aecb3d1a065fe05c295992689398440361eb373e
Pedro Donoso <pedro.donoso.r@usach.cl>
2024-11-22 00:58:23 -0300

    agrego boton

--- App.js
+++ App.js
@@ -1,6 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 //componentes
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { 
+  StyleSheet, 
+  Text, View, 
+  TextInput, 
+  TouchableOpacity 
+} from 'react-native';
 
 const styles = StyleSheet.create({
 
@@ -11,7 +16,8 @@ const styles = StyleSheet.create({
 
   title: {
     fontSize: 20,
-    color: '#6f6f6f'
+    color: '#6f6f6f',
+    marginStart: 20
   },
   text: {
     fontSize: 16,
@@ -25,14 +31,20 @@ const styles = StyleSheet.create({
     borderColor: '#6f6f6f',
     borderWidth: 1
   },
+  inputContainer: {
+    margin: 20
+  }
 });
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Mis Tareas por hacer</Text>
-      <View>
+      <View style={styles.inputContainer}>
           <TextInput style={styles.textInput} />
+          <TouchableOpacity>
+            <Text>Agregar</Text>
+          </TouchableOpacity>
       </View>
     </View>
   );
