170cff977523458222594ef3d4141e5f4a9853a8
pedro-donoso <pedro.donoso.r@usach.cl>
2024-11-22 21:18:56 -0300

    agrego tarea

--- App.tsx
+++ App.tsx
@@ -8,26 +8,6 @@ import {
 } from "react-native";
 import styles from "./Styles";
 import RenderItem from "./RenderItem";
-
-//tareas
-const tasks = [
-  {
-    title: "Alimentar al perro",
-    done: true,
-    date: new Date(),
-  },
-  {
-    title: "Salir a correr",
-    done: false,
-    date: new Date(),
-  },
-  {
-    title: "Nueva tarea",
-    done: true,
-    date: new Date(),
-  },
-];
-
 export interface Task {
   title: string;
   done: Boolean;
@@ -35,7 +15,13 @@ export interface Task {
 }
 
 export default function App() {
-  const [text, setText] = useState('Hola mundo');
+  const [text, setText] = useState('');
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  const addTask = () => {
+    
+  }
+
   const markDone = () => {
     console.log('markDone');
   };
@@ -48,8 +34,11 @@ export default function App() {
     <View style={styles.container}>
       <Text style={styles.title}>Mis Tareas por hacer</Text>
       <View style={styles.inputContainer}>
-        <TextInput placeholder="Ingresa nueva tarea" value={text} style={styles.textInput} />
-        <TouchableOpacity style={styles.addButton}>
+        <TextInput placeholder="Ingresa nueva tarea" 
+        onChangeText={(t: string) => setText(t)}
+        value={text} 
+        style={styles.textInput} />
+        <TouchableOpacity onPress={addTask} style={styles.addButton}>
           <Text style={styles.whitetext}>Agregar</Text>
         </TouchableOpacity>
       </View>
